298453d6aa7d1f14793b278454a99a333a5716e9
Maria Sol Arroyo <mariasolarroyoc@gmail.com>
2025-03-13 17:12:06 -0300

    v1

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,16 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['react-router-dom'],
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,8 +18,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    // No es necesario marcar react-router-dom como external
     rollupOptions: {
-      external: ['react-router-dom'],
+      // external: ['react-router-dom'], // Elimina esta línea
     },
   },
 })
